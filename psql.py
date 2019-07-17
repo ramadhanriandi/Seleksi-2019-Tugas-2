dabc72ad41ec0ab99c5b269f1cf3ca7552a1ded9
ramadhanriandi <izurbudianto@gmail.com>
2019-07-17 10:08:16 +0700

    commit query into psql using python

--- psql.py
+++ psql.py
@@ -16,17 +16,58 @@ obj = json.loads(data)
 
 cursor = connection.cursor()
 
-# create_nation_table_query = '''CREATE TABLE nation
-#   (ID INT PRIMARY KEY   NOT NULL,
-#   NAME        TEXT      NOT NULL);'''
-# cursor.execute(create_nation_table_query)
+create_nation_table_query = '''CREATE TABLE recap
+  (
+    ID INT PRIMARY KEY      NOT NULL,
+    COUNTRY         TEXT    NOT NULL,
+    APR_04_2019     INT     NULL,
+    FEB_07_2019     INT     NULL,
+    DEC_20_2018     INT     NULL,
+    NOV_29_2018     INT     NULL,
+    OCT_25_2018     INT     NULL,
+    SEP_20_2018     INT     NULL,
+    AUG_16_2018     INT     NULL,
+    JUNE_07_2018    INT     NULL,
+    MAY_17_2018     INT     NULL,
+    APR_12_2018     INT     NULL
+  );'''
+cursor.execute(create_nation_table_query)
 
 i = 0
 for data in obj['data']:
-  postgres_insert_query = """INSERT INTO nation (ID, NAME) VALUES (%s,%s)"""
+  postgres_insert_query = """INSERT INTO recap (ID, COUNTRY) VALUES (%s,%s)"""
   record_to_insert = (i, data['name'])
   cursor.execute(postgres_insert_query, record_to_insert)
   i += 1
+  postgres_update_query = """
+    UPDATE recap
+    SET
+    APR_04_2019 = %s,
+    FEB_07_2019 = %s,
+    DEC_20_2018 = %s,
+    NOV_29_2018 = %s,
+    OCT_25_2018 = %s,
+    SEP_20_2018 = %s,
+    AUG_16_2018 = %s,
+    JUNE_07_2018 = %s,
+    MAY_17_2018 = %s,
+    APR_12_2018 = %s
+    WHERE COUNTRY = %s;
+    """
+  record_to_update = (
+    data['recap'][0]['point'],
+    data['recap'][1]['point'],
+    data['recap'][2]['point'],
+    data['recap'][3]['point'],
+    data['recap'][4]['point'],
+    data['recap'][5]['point'],
+    data['recap'][6]['point'],
+    data['recap'][7]['point'],
+    data['recap'][8]['point'],
+    data['recap'][9]['point'],
+    data['name']
+  )
+  cursor.execute(postgres_update_query, record_to_update)
 
 connection.commit()
 
